bcdb8996a22190d69155b3c221ee9eb13b3616e4
Oleg Kuchumov <voenniy@gmail.com>
2016-04-09 18:15:49 +0300

    new jsonrpc generator

--- components/JsonRPC.php
+++ components/JsonRPC.php
@@ -21,17 +21,37 @@ class JsonRPC  {
         $this->method = ['method'=>$method, 'params' => $params];
     }
 
-    public function __toString(){
-        $s = '';
-        if($this->method){
+    public static function toString($method, $params){
+        $method = ['method' => $method, 'params' => $params];
+        return self::generate($method);
+    }
+
+    public static function toArray($method, $params){
+        $method = ['method' => $method, 'params' => $params];
+        return self::generate($method, false);
+    }
+
+    protected static function generate($method, $asString = true){
+        $request = [];
+        if(!isset($method['params'])){
+            $method['params'] = [];
+        }
+        if(!is_array($method['params'])){
+            $method['params'] = [$method['params']];
+        }
+        if($method) {
             $request = [
                 'jsonrpc' => '2.0',
-                'method' => $this->method['method'],
-                'params' => $this->method['params'],
+                'method' => $method['method'],
+                'params' => $method['params'],
                 'id' => uniqid()
             ];
-            $s .= json_encode($request);
         }
+        return $asString ? json_encode($request) : $request;
+    }
+
+    public function __toString(){
+        $s = self::generate($this->method);
         $this->method = null;
         return $s;
     }
