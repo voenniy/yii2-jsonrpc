377da84e6297b11a151c19c2d1a5ed0047bd9dc2
Oleg Kuchumov <voenniy@gmail.com>
2016-07-24 15:24:58 +0300

    перенес стабильную версию textcoplete в репозиторий (убрал из зависимостей)

--- assets/JsonrpcAsset.php
+++ assets/JsonrpcAsset.php
@@ -8,12 +8,12 @@ class JsonrpcAsset extends AssetBundle {
     public $sourcePath = '@voenniy/jsonrpc/assets/js';
 
     public $js = [
+        'jquery.textcomplete.min.js',
         'rpc_callback.js',
         'jquery.jsonrpc.js',
         'script.js'
     ];
     public $depends = [
-        'voenniy\jsonrpc\assets\JqueryAutocompleteAsset',
         'yii\web\JqueryAsset',
     ];
 } 
